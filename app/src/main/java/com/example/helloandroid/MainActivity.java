15fdc1a290434d821909bacf501c05903d5f3278
totaeza31 <47884416+totaeza31@users.noreply.github.com>
2019-12-02 19:42:01 +0700

    กดเเล้วลิ้งไปอีกหน้า

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.helloandroid;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +21,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -62,24 +68,46 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
+
+                            setContentView(R.layout.activity_list);
+
+
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
-                            updateUI(null);
+
+                            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                            alertDialog.setTitle("Please input username,password");
+                            alertDialog.setMessage("id:test@test.com password:testtest");
+                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                            dialog.dismiss();
+
+                                        }
+                                    });
+                            alertDialog.show();
+
+
                         }
                     }
                 });
     }
+
+
+
+
 
     public void sigoutClick(View view){
         final String TAG = "sigoutClick";
         Log.w(TAG, "sign out");
         mAuth.signOut();
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+
     }
 
     @Override
@@ -88,20 +116,9 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            updateUI(currentUser);
+
         }
     }
 
-    public void updateUI(FirebaseUser currentUser){
-        final String TAG = "updateUI";
-        TextView loginName = findViewById(R.id.textView);
-        if(currentUser != null){
-            Log.d(TAG, currentUser.getEmail());
 
-            loginName.setText(currentUser.getEmail());
-        }
-        else{
-            loginName.setText("");
-        }
-    }
 }
